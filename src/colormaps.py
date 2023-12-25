bbaf01f329a4775af483a9acaa95e2498d55a5f0
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2023-12-24 16:52:33 -0800

    Create colormaps.py

---
+++ src/colormaps.py
@@ -0,0 +1,12 @@
+import matplotlib.colors
+
+def temperature_colormap():
+    temperature_color_map = matplotlib.colors.LinearSegmentedColormap.from_list("temperature", ["magenta", "darkviolet", "blue", "deepskyblue", "yellow", "mistyrose", "pink", "red", "darkred"])
+
+    return temperature_color_map
+
+
+def relative_humidity_colormap():
+    relative_humidity_colormap = matplotlib.colors.LinearSegmentedColormap.from_list("relative humidity", ["saddlebrown", "darkorange", "gold", "lightgoldenrodyellow", "yellowgreen", "lawngreen", "springgreen", "lime"])
+    
+    return relative_humidity_colormap
