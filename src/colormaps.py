3911cb40ff43cabc54b1e484972a54dd928bfbaf
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2023-12-29 09:59:29 -0800

    Update colormaps.py

--- src/colormaps.py
+++ src/colormaps.py
@@ -10,9 +10,16 @@ def relative_humidity_colormap():
     relative_humidity_colormap = matplotlib.colors.LinearSegmentedColormap.from_list("relative humidity", ["saddlebrown", "darkorange", "gold", "lightgoldenrodyellow", "yellowgreen", "lawngreen", "springgreen", "lime"])
     
     return relative_humidity_colormap
-    
-    
+
+
 def relative_humidity_change_colormap():
     relative_humidity_change_colormap = matplotlib.colors.LinearSegmentedColormap.from_list("relative humidity", ["saddlebrown", "darkorange", "gold", "lightgoldenrodyellow", "white", "yellowgreen", "lawngreen", "springgreen", "lime"])
     
     return relative_humidity_change_colormap
+
+
+def excellent_recovery_colormap():
+    excellent_recovery_colormap = matplotlib.colors.LinearSegmentedColormap.from_list("relative humidity", ["greenyellow", "lightgreen", "palegreen", "mediumspringgreen", "springgreen", "lime"])
+    
+    return excellent_recovery_colormap
+
