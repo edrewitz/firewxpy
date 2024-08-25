35a584254662d6603aafa81c9a5d0e18c6db156d
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2024-08-25 13:54:25 -0700

    Update firewxpy.py

--- src/firewxpy.py
+++ src/firewxpy.py
@@ -1,6 +1,6 @@
-import NWS_Forecast_Relative_Humidity_Graphics_CONUS
-import NWS_Temperature_Forecast_Graphics_CONUS
-import Real_Time_Mesoscale_Analysis_Graphics_CONUS
+#import NWS_Forecast_Relative_Humidity_Graphics_CONUS
+#import NWS_Temperature_Forecast_Graphics_CONUS
+#import Real_Time_Mesoscale_Analysis_Graphics_CONUS
 import standard
 import SPC_Outlook_Graphics
 import calc
