5f5130c16586192d1718cc639d7ff5d0aeaae90a
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2024-09-01 20:11:35 -0700

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,2 @@
-from .NWS_Forecast_Graphics import temperature
-from .NWS_Forecast_Graphics import relative_humidity
-import standard
-import calc
-import colormaps
-import data_access
-import geometry
-import parsers
+import firewxpy
 
