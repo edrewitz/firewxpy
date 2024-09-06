1585f0e620e7a7f69b71e8e063fe59b3b6b1d109
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2024-09-06 11:11:27 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,27 @@ from setuptools import setup, find_packages
 if sys.version_info[0] < 3:
   print("ERROR: User is running Python 2.7.\nTo use FireWxPy, the user must be using Python 3.")
 
-setup()
+setup(
+    name = "__init__.py",
+    version = "1.0.0",
+    packages = ['src', '__init__.py'],
+    install_requires=[
+        "matplotlib>=3.7",
+        "metpy>=1.5.1",
+        "netcdf4>=1.7.1",
+        "numpy>=1.24",
+        "pandas>=2",
+        "siphon>=0.9",
+        "xarray>=2023.1.0",
+        "pysolar>=0.11",
+        "pygrib>=2.1.4",
+        "cfgrib>=0.9.10.4",
+        "cartopy>=0.21.0",
+         "imageio>=2.34.0",
+    ],
+    author="Eric J. Drewitz",
+    description="Provides automated weather graphics with a focus on fire weather.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown"
+
+)
