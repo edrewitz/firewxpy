1083134865cc87e7caae1745de1c78522e6e4b73
Eric Drewitz <s2s.forecaster@gmail.com>
2024-09-06 19:35:07 -0700

    test

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info[0] < 3:
 setup(
     name = "firewxpy",
     version = "1.0.0.3",
-    packages = find_packages('firewxpy'),
+    packages = find_packages(),
     install_requires=[
         "matplotlib>=3.7",
         "metpy>=1.5.1",
