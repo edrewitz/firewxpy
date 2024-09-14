cb7c513cc952ab87a90cabd178707672b9a4cc96
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2024-09-14 15:56:05 -0700

    Update observations_graphics.py

--- firewxpy/observations_graphics.py
+++ firewxpy/observations_graphics.py
@@ -12,28 +12,31 @@ This file was written by Meteorologist Eric J. Drewitz
 
 import pytz
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 import cartopy.crs as ccrs
 import cartopy.feature as cfeature
 import metpy.plots as mpplots
 import metpy.calc as mpcalc
 import numpy as np
-import parsers
-import data_access as da
-import geometry
-import calc
-import colormaps
+import firewxpy.parsers
+import firewxpy.data_access as da
+import firewxpy.calc
+import firewxpy.colormaps
 import pandas as pd
 import matplotlib.gridspec as gridspec
 import matplotlib.dates as md
-import standard
+import firewxpy.standard
 
 from metpy.plots import USCOUNTIES
 from datetime import datetime, timedelta
 from metpy.plots import colortables
 from dateutil import tz
 from pysolar import solar, radiation
+from firewxpy.utilities import file_functions
 
-def graphical_daily_summary(station_id, file_save_path=None):
+mpl.rcParams['font.weight'] = 'bold'
+
+def graphical_daily_summary(station_id):
 
 
     r'''
@@ -128,7 +131,7 @@ def graphical_daily_summary(station_id, file_save_path=None):
     ax0.set_ylim(minimum_temperature - 2, maximum_temperature + 2)
     ax0.set_xlabel("Hour", color='white', fontsize=8, fontweight='bold')
     ax0.set_ylabel("Temperature (\N{DEGREE SIGN}F)", color='white', fontsize=8, fontweight='bold')
-    ax0.set_title("Temperature", color='white', fontsize=10, fontweight='bold')
+    ax0.set_title("Temperature", color='white', fontsize=11, fontweight='bold')
     ax0.tick_params(axis='x', colors='white')
     ax0.tick_params(axis='y', colors='white')
     
@@ -138,7 +141,7 @@ def graphical_daily_summary(station_id, file_save_path=None):
     ax1.set_ylim(minimum_relative_humidity - 5, maximum_relative_humidity + 5)
     ax1.set_xlabel("Hour", color='white', fontsize=8, fontweight='bold')
     ax1.set_ylabel("Relative Humidity (%)", color='white', fontsize=8, fontweight='bold')
-    ax1.set_title("Relative Humidity", color='white', fontsize=10, fontweight='bold')
+    ax1.set_title("Relative Humidity", color='white', fontsize=11, fontweight='bold')
     ax1.tick_params(axis='x', colors='white')
     ax1.tick_params(axis='y', colors='white')
     
@@ -148,7 +151,7 @@ def graphical_daily_summary(station_id, file_save_path=None):
     ax2.set_ylim(0, maximum_wind_speed + 2)
     ax2.set_xlabel("Hour", color='white', fontsize=8, fontweight='bold')
     ax2.set_ylabel("Wind Speed (MPH)", color='white', fontsize=8, fontweight='bold')
-    ax2.set_title("Wind Speed", color='white', fontsize=10, fontweight='bold')
+    ax2.set_title("Wind Speed", color='white', fontsize=11, fontweight='bold')
     ax2.tick_params(axis='x', colors='white')
     ax2.tick_params(axis='y', colors='white')
     
@@ -157,7 +160,7 @@ def graphical_daily_summary(station_id, file_save_path=None):
     ax3.xaxis.set_major_formatter(md.DateFormatter('%H', tz=to_zone))
     ax3.set_xlabel("Hour", color='white', fontsize=8, fontweight='bold')
     ax3.set_ylabel("Solar Elevation Angle (Degrees)", color='white', fontsize=8, fontweight='bold')
-    ax3.set_title("Solar Elevation Angle", color='white', fontsize=10, fontweight='bold')
+    ax3.set_title("Solar Elevation Angle", color='white', fontsize=11, fontweight='bold')
     ax3.axhline(0, c='black', linestyle='--', linewidth=2)
     fig.text(0.55, 0.855, "Elevation Angle > 0 = Day\nElevation Angle < 0 = Night", fontsize=9, fontweight='bold')
     ax3.tick_params(axis='x', colors='white')
@@ -168,65 +171,55 @@ def graphical_daily_summary(station_id, file_save_path=None):
     ax4.xaxis.set_major_formatter(md.DateFormatter('%H', tz=to_zone))
     ax4.set_xlabel("Hour", color='white', fontsize=8, fontweight='bold')
     ax4.set_ylabel("Solar Radiation (W/m^2)", color='white', fontsize=8, fontweight='bold')
-    ax4.set_title("Solar Radiation", color='white', fontsize=10, fontweight='bold')
+    ax4.set_title("Solar Radiation", color='white', fontsize=11, fontweight='bold')
     ax4.tick_params(axis='x', colors='white')
     ax4.tick_params(axis='y', colors='white')
-    
-    fig.text(0.6, 0.30, "Daily Weather Summary", color='white', fontsize=16, fontweight='bold')
-    
-    try:
-        maximum_temperature = int(round(maximum_temperature, 0))
-        fig.text(0.483, 0.28, "Maximum Temperature: " + str(maximum_temperature) +" (\N{DEGREE SIGN}F) " + maximum_temperature_time_local.strftime('%H:%M Local') + " ("+ maximum_temperature_time.strftime('%H:%M UTC')+")", color='white', fontsize=14, fontweight='bold')
-        
-    except Exception as a:
-        fig.text(0.483, 0.28, "Maximum Temperature: Not Available", color='white', fontsize=14, fontweight='bold')
-    
-    try:
-        minimum_temperature = int(round(minimum_temperature, 0))
-        fig.text(0.483, 0.26, "Minimum Temperature: " + str(minimum_temperature) +" (\N{DEGREE SIGN}F) "+ minimum_temperature_time_local.strftime('%H:%M Local') + " ("+ minimum_temperature_time.strftime('%H:%M UTC')+")", color='white', fontsize=14, fontweight='bold')
-    except Exception as b:
-        fig.text(0.483, 0.26, "Minimum Temperature: Not Available", color='white', fontsize=14, fontweight='bold')
-    
-    try:
-        maximum_relative_humidity = int(round(maximum_relative_humidity, 0))
-        fig.text(0.483, 0.24, "Maximum RH: " + str(maximum_relative_humidity) +" (%) "+ maximum_relative_humidity_time_local.strftime('%H:%M Local') + " ("+ maximum_relative_humidity_time.strftime('%H:%M UTC')+")", color='white', fontsize=14, fontweight='bold')
-    except Exception as c:
-        fig.text(0.483, 0.24, "Maximum RH: Not Available", color='white', fontsize=14, fontweight='bold')
-    
-    try:
-        minimum_relative_humidity = int(round(minimum_relative_humidity, 0))
-        fig.text(0.483, 0.22, "Minimum RH: " + str(minimum_relative_humidity) +" (%) "+ minimum_relative_humidity_time_local.strftime('%H:%M Local') + " ("+ minimum_relative_humidity_time.strftime('%H:%M UTC')+")", color='white', fontsize=14, fontweight='bold')
-    except Exception as d:
-        fig.text(0.483, 0.22, "Minimum RH: Not Available", color='white', fontsize=14, fontweight='bold')
-    
-    try:
-        maximum_wind_speed = int(round(maximum_wind_speed, 0))
-        fig.text(0.483, 0.20, "Maximum Wind Speed: " + str(maximum_wind_speed) +" (MPH) "+ maximum_wind_speed_time_local.strftime('%H:%M Local') + " ("+ maximum_wind_speed_time.strftime('%H:%M UTC')+")", color='white', fontsize=14, fontweight='bold')
-    except Exception as e:
-        fig.text(0.483, 0.20, "Maximum Wind Speed: Not Available", color='white', fontsize=14, fontweight='bold')
-    
-    try:
-        maximum_wind_gust = int(round(maximum_wind_gust, 0))
-        fig.text(0.483, 0.18, "Maximum Wind Gust: " + str(maximum_wind_gust) +" (MPH) "+ maximum_wind_gust_time_local.strftime('%H:%M Local') + " ("+ maximum_wind_gust_time.strftime('%H:%M UTC')+")", color='white', fontsize=14, fontweight='bold')
-    except Exception as f:
-        fig.text(0.483, 0.18, "Maximum Wind Gust: Not Available (No Gusts Reported)", color='white', fontsize=14, fontweight='bold')
-    
-    fig.text(0.625, 0.15, "Solar Information", color='white', fontsize=16, fontweight='bold')
-    if max_elevation >= 0:
-        fig.text(0.483, 0.13, "Maximum Elevation: " + str(round(max_elevation, 1)) + " Degrees Above the Horizon", color='white', fontsize=14, fontweight='bold')
-    if max_elevation < 0:
-        fig.text(0.483, 0.13, "Maximum Elevation: " + str(round(max_elevation, 1)) + " Degrees Below the Horizon", color='white', fontsize=14, fontweight='bold')
-    fig.text(0.483, 0.11, "Maximum Solar Radiation: " + str(round(max_radiation, 1)) + " (W/m^2)", color='white', fontsize=14, fontweight='bold')
-    
+
     local_time, utc_time = standard.plot_creation_time()
 
     props = dict(boxstyle='round', facecolor='wheat', alpha=1)
 
-    fig.text(0.025, 0.02, "Plot Created With FireWxPy (C) Eric J. Drewitz 2024\nData Source: thredds.ucar.edu\nImage Created: " + local_time.strftime('%m/%d/%Y %H:%M Local') + " (" + utc_time.strftime('%H:%M UTC') + ")", fontsize=18, fontweight='bold', verticalalignment='top', bbox=props, zorder=10)    
+    try:
+        maximum_temperature = int(round(maximum_temperature, 0))
+        maximum_temperature = str(maximum_temperature)
+    except Exception as e:
+        maximum_temperature = 'NA'
+    try:
+        minimum_temperature = int(round(minimum_temperature, 0))
+        minimum_temperature = str(minimum_temperature)
+    except Exception as e:
+        minimum_temperature = 'NA'
+    try:
+        maximum_relative_humidity = int(round(maximum_relative_humidity, 0))
+        maximum_relative_humidity = str(maximum_relative_humidity)
+    except Exception as e:
+        maximum_relative_humidity = 'NA'
+    try:
+        minimum_relative_humidity = int(round(minimum_relative_humidity, 0))
+        minimum_relative_humidity = str(minimum_relative_humidity)
+    except Exception as e:
+        minimum_relative_humidity = 'NA'
+    try:
+        maximum_wind_speed = int(round(maximum_wind_speed, 0))
+        maximum_wind_speed = str(maximum_wind_speed)
+    except Exception as e:
+        maximum_wind_speed = 'NA'
+    try:
+        maximum_wind_gust = int(round(maximum_wind_gust, 0))
+        maximum_wind_gust = str(maximum_wind_gust)
+    except Exception as e:
+        maximum_wind_gust = 'NA'
 
-    if file_save_path == None:
-        return fig
-    if file_save_path != None:
-        parsers.save.save_image(file_save_path)
+        if maximum_wind_gust != 'NA':
+
+            fig.text(0.487, 0.13, "Maximum Temperature: " +maximum_temperature +" (\N{DEGREE SIGN}F) " + maximum_temperature_time_local.strftime('%H:%M Local') + " ("+ maximum_temperature_time.strftime('%H:%M UTC')+")\n\nMinimum Temperature: " + minimum_temperature +" (\N{DEGREE SIGN}F) "+ minimum_temperature_time_local.strftime('%H:%M Local') + " ("+ minimum_temperature_time.strftime('%H:%M UTC')+")\n\nMaximum RH: " + maximum_relative_humidity +" (%) "+ maximum_relative_humidity_time_local.strftime('%H:%M Local') + " ("+ maximum_relative_humidity_time.strftime('%H:%M UTC')+")\n\nMinimum RH: " + minimum_relative_humidity +" (%) "+ minimum_relative_humidity_time_local.strftime('%H:%M Local') + " ("+ minimum_relative_humidity_time.strftime('%H:%M UTC')+")\n\nMaximum Wind Speed: " + maximum_wind_speed +" (MPH) "+ maximum_wind_speed_time_local.strftime('%H:%M Local') + " ("+ maximum_wind_speed_time.strftime('%H:%M UTC')+")\n\nMaximum Wind Gust: " + maximum_wind_gust +" (MPH) "+ maximum_wind_gust_time_local.strftime('%H:%M Local') + " ("+ maximum_wind_gust_time.strftime('%H:%M UTC')+")\n\nMaximum Elevation: " + str(round(max_elevation, 1)) + " Degrees Above the Horizon\n\nMinimum Elevation: " + str(round(min_elevation, 1)) + " Degrees Below the Horizon\n\nMaximum Solar Radiation: " + str(round(max_radiation, 1)) + " (W/m^2)", color='black', fontsize=11, fontweight='bold', bbox=props, zorder=10)
+
+        if maximum_wind_gust == 'NA':
+
+            fig.text(0.487, 0.13, "Maximum Temperature: " +maximum_temperature +" (\N{DEGREE SIGN}F) " + maximum_temperature_time_local.strftime('%H:%M Local') + " ("+ maximum_temperature_time.strftime('%H:%M UTC')+")\n\nMinimum Temperature: " + minimum_temperature +" (\N{DEGREE SIGN}F) "+ minimum_temperature_time_local.strftime('%H:%M Local') + " ("+ minimum_temperature_time.strftime('%H:%M UTC')+")\n\nMaximum RH: " + maximum_relative_humidity +" (%) "+ maximum_relative_humidity_time_local.strftime('%H:%M Local') + " ("+ maximum_relative_humidity_time.strftime('%H:%M UTC')+")\n\nMinimum RH: " + minimum_relative_humidity +" (%) "+ minimum_relative_humidity_time_local.strftime('%H:%M Local') + " ("+ minimum_relative_humidity_time.strftime('%H:%M UTC')+")\n\nMaximum Wind Speed: " + maximum_wind_speed +" (MPH) "+ maximum_wind_speed_time_local.strftime('%H:%M Local') + " ("+ maximum_wind_speed_time.strftime('%H:%M UTC')+")\n\nMaximum Elevation: " + str(round(max_elevation, 1)) + " Degrees Above the Horizon\n\nMinimum Elevation: " + str(round(min_elevation, 1)) + " Degrees Below the Horizon\n\nMaximum Solar Radiation: " + str(round(max_radiation, 1)) + " (W/m^2)", color='black', fontsize=11, fontweight='bold', bbox=props, zorder=10)
+    
+    fig.text(0.27, 0.07, "Plot Created With FireWxPy (C) Eric J. Drewitz 2024\nData Source: thredds.ucar.edu\nImage Created: " + local_time.strftime('%m/%d/%Y %H:%M Local') + " (" + utc_time.strftime('%H:%M UTC') + ")", fontsize=14, fontweight='bold', verticalalignment='top', bbox=props, zorder=10)    
+
+    file_functions.save_daily_weather_summary(fig, station_id)
 
     
