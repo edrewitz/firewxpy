a0b181f3f87124fbdb8d6012a29a1e4599d3c033
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2024-09-14 15:58:35 -0700

    Update __init__.py

--- firewxpy/__init__.py
+++ firewxpy/__init__.py
@@ -1,3 +1,5 @@
 import firewxpy.RTMA_Graphics as rtma
 from firewxpy.NWS_Forecast_Graphics import temperature as nws_temperature_forecast
 from firewxpy.NWS_Forecast_Graphics import relative_humidity as nws_relative_humidity_forecast
+from firewxpy.observations import graphical_daily_summary
+from firewxpy.soundings import plot_observed_sounding
