fbcd581945d4ba3456ded9b8bebb22bf1c27f886
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2024-09-28 12:09:07 -0700

    Update RTMA_Graphics.py

--- firewxpy/RTMA_Graphics.py
+++ firewxpy/RTMA_Graphics.py
@@ -14,6 +14,7 @@ import matplotlib.gridspec as gridspec
 import math
 import firewxpy.settings as settings
 import firewxpy.standard as standard
+import firewxpy.dims as dims
 
 
 from matplotlib.patheffects import withStroke
