3032f8a3460c06e88c0526831bc0bd8dd2acba69
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2024-10-19 13:46:11 -0700

    Update NWS_Forecast_Graphics.py

--- firewxpy/NWS_Forecast_Graphics.py
+++ firewxpy/NWS_Forecast_Graphics.py
@@ -32,7 +32,7 @@ import firewxpy.settings as settings
 import firewxpy.standard as standard
 import os
 import imageio
-import dims
+import firewxpy.dims
 
 from metpy.plots import USCOUNTIES
 from datetime import datetime, timedelta
