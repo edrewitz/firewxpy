07fa7dca680dc679f3216be5bd96c6fa501e41ca
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2024-12-01 19:26:30 -0800

    Update __init__.py

--- firewxpy/__init__.py
+++ firewxpy/__init__.py
@@ -1,9 +1,9 @@
-import firewxpy.RTMA_Graphics as rtma
+import firewxpy.RTMA_Graphics_CONUS as rtma_conus
 import firewxpy.SPC_Outlook_Graphics as spc
 import firewxpy.standard as standard
 from firewxpy.data_access import RTMA_CONUS, NDFD_CONUS
-from firewxpy.NWS_Forecast_Graphics import temperature as nws_temperature_forecast
-from firewxpy.NWS_Forecast_Graphics import relative_humidity as nws_relative_humidity_forecast
+from firewxpy.NWS_Forecast_Graphics import temperature as nws_temperature_forecast_conus
+from firewxpy.NWS_Forecast_Graphics import relative_humidity as nws_relative_humidity_forecast_conus
 from firewxpy.NWS_Forecast_Graphics import dry_and_windy as nws_dry_and_windy_forecast
 from firewxpy.observations import graphical_daily_summary
 from firewxpy.soundings import plot_observed_sounding, plot_observed_sounding_custom_date_time
