72a276bfda074e38f5c8e8d8c17fe060405f6a21
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2024-12-21 15:14:43 -0800

    Update solar_information.py

--- firewxpy/solar_information.py
+++ firewxpy/solar_information.py
@@ -29,6 +29,26 @@ def get_solar_midnight(times_list, solar_elevation):
             return i 
 
 def plot_daily_solar_information(latitude, longitude):
+    
+    r'''
+    This function plots a daily solar graph and gives other types of solar information. 
+
+    Required Arguments: 1) latitude (Float) - The latitude coordinate in decimal degrees format. 
+                     2) longitude (Float) - The longitude coordinate in decimal degrees format. 
+
+    Optional Arguments: None
+
+    Return: A graphic showing the following information saved to f:Weather Data/Solar Information:
+
+    1) Daily solar elevation graphic
+    2) A data table showing the following information:
+        i) Maximum Daily Solar Elevation (solar-noon sun angle)
+        ii) Minimum Daily Solar Elevation (solar-midnight sun angle)
+        iii) Difference in daily solar elevation between current day, equinox, summer solstice and winter solstice
+        iv) Total Daily Solar Radiation [W/m^2]
+
+
+    '''
 
 
     latitude, longitude = latitude, longitude
