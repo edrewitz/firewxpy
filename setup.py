8f3970e5680017c671313017988c515e601ef06b
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2025-02-09 20:45:28 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ if sys.version_info[0] < 3:
 
 setup(
     name = "firewxpy",
-    version = "1.4.4",
+    version = "1.4.4.1",
     packages = find_packages(),
     install_requires=[
         "matplotlib>=3.7",
