071abb9c460c065d80d84f44dcc3a5bc519e2931
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2025-04-24 19:14:32 -0700

    Update fuels_graphics.py

--- firewxpy/fuels_graphics.py
+++ firewxpy/fuels_graphics.py
@@ -1,3 +1,4 @@
+
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import matplotlib.dates as md
@@ -6,18 +7,22 @@ import pandas as pd
 import shutil
 
 from data_access import FEMS
-from raws_sigs import get_psa_percentiles, sort_data_by_zone
+from raws_sigs import get_psa_percentiles, station_stats, sort_data_by_psa, get_stats, get_psa_climatology
 from standard import plot_creation_time
 from datetime import datetime, timedelta, UTC
 from utilities import file_functions
+from calendar import isleap
 
 local_time, utc_time = plot_creation_time()
 
 mpl.rcParams['font.weight'] = 'bold'
 props = dict(boxstyle='round', facecolor='wheat', alpha=1)
+date_box = dict(boxstyle='round', facecolor='dimgrey', alpha=1)
 
 mpl.rcParams['xtick.labelsize'] = 9
 mpl.rcParams['ytick.labelsize'] = 9
+
+plt.style.use("ggplot")
 
 def create_psa_100hr_fuels_charts(gacc_region, number_of_years_for_averages=15, fuel_model='Y', start_date=None, data=False):
     
@@ -32,15 +37,24 @@ def create_psa_100hr_fuels_charts(gacc_region, number_of_years_for_averages=15, 
         pass
         
     get_psa_percentiles(gacc_region)
-    sort_data_by_zone(gacc_region)
+    station_stats(gacc_region)
+    get_stats(gacc_region)
+    get_psa_climatology(gacc_region)
+    sort_data_by_psa(gacc_region)
 
     data_dir = f"FEMS Data/{gacc_region}/PSA Data"
     percentiles_dir = f"FEMS Data/{gacc_region}/PSA Percentiles"
+    climo_avg_dir = f"FEMS Data/{gacc_region}/PSA Climo/AVG"
+    climo_min_dir = f"FEMS Data/{gacc_region}/PSA Climo/MIN"
 
     percentiles = pd.read_csv(f"FEMS Data/{gacc_region}/PSA Percentiles/PSA_Percentiles.csv")
 
+    leap = isleap(utc_time.year)
     if start_date == None:
-        days = number_of_years_for_averages * 365
+        if leap == False:
+            days = number_of_years_for_averages * 365
+        else:
+            days = number_of_years_for_averages * 366
         start_date = utc_time - timedelta(days=days)
         start_year = start_date.year
         xmin = start_date
@@ -61,43 +75,168 @@ def create_psa_100hr_fuels_charts(gacc_region, number_of_years_for_averages=15, 
     else:
         pass
 
+    if os.path.exists(f"{data_dir}/.ipynb_checkpoints"):
+        shutil.rmtree(f"{data_dir}/.ipynb_checkpoints")
+    else:
+        pass
+
     files = os.listdir(f"{data_dir}")
     psa = 1
     for i in range(0, len(files)):
 
         fname = f"PSA {psa}.png"
 
-        #try:
-        df = pd.read_csv(f"{data_dir}/{files[i]}") 
+        df_data = pd.read_csv(f"{data_dir}/{files[i]}") 
+        df_climo_avg = pd.read_csv(f"{climo_avg_dir}/{files[i]}") 
+        df_climo_min = pd.read_csv(f"{climo_min_dir}/{files[i]}") 
 
-        dates = pd.to_datetime(df['date'])
+        dates = pd.to_datetime(df_data['dates'])
 
         fig = plt.figure(figsize=(12,12))
 
         ax = fig.add_subplot(1, 1, 1)
 
         ax.xaxis.set_major_formatter(md.DateFormatter('%m-%d'))
-        plt.title(f"{gacc_region} 100-HR Dead Fuel Moisture: Zone {psa}", fontsize=12, fontweight='bold', loc='left')
+        plt.title(f"{gacc_region} 100-HR Dead Fuel Moisture: PSA {psa}", fontsize=12, fontweight='bold', loc='left')
         plt.title(f"Period Of Record: {start_year} - {utc_time.year}", fontsize=10, fontweight='bold', loc='right')
+        ax.text(0.01, -0.05, "Plot Created With FireWxPy (C) Eric J. Drewitz " +utc_time.strftime('%Y')+" | Data Source: USDA/FEMS", transform=ax.transAxes, fontsize=8, fontweight='bold', bbox=props)
+        ax.text(0.405, 0.98, f"Valid Date: {dates.iloc[-2].strftime("%m/%d/%Y")}", transform=ax.transAxes, fontsize=8, color='white', fontweight='bold', bbox=date_box)
 
-        #ax.hlines(percentiles['100hr_DFM_3_percentile'].iloc[i], xmin=xmin, xmax=xmax, color='black', linestyle='--', alpha=0.5, zorder=2)
-        #ax.hlines(percentiles['100hr_DFM_10_percentile'].iloc[i], xmin=xmin, xmax=xmax, color='black', linestyle='--', alpha=0.5, zorder=2)
-        #ax.hlines(percentiles['100hr_DFM_20_percentile'].iloc[i], xmin=xmin, xmax=xmax, color='black', linestyle='--', alpha=0.5, zorder=2)
-        #ax.hlines(percentiles['100hr_DFM_40_percentile'].iloc[i], xmin=xmin, xmax=xmax, color='black', linestyle='--', alpha=0.5, zorder=2)
+        if leap == True:
+            jmax = 366
+        else:
+            jmax = 365
+        
+        ax.set_xlim(0, jmax)
+        ax.set_ylim(0, 30)
+        
+        ax.hlines(percentiles['100hr_DFM_3_percentile'].iloc[i], xmin=0, xmax=jmax, color='black', linestyle='-', alpha=0.5, zorder=2, label=f"3rd PERCENTILE")
+        ax.hlines(percentiles['100hr_DFM_10_percentile'].iloc[i], xmin=0, xmax=jmax, color='black', linestyle='--', alpha=0.5, zorder=2, label=f"10th PERCENTILE")
+        ax.hlines(percentiles['100hr_DFM_20_percentile'].iloc[i], xmin=0, xmax=jmax, color='black', linestyle='-.', alpha=0.5, zorder=2, label=f"20th PERCENTILE")
+        ax.hlines(percentiles['100hr_DFM_40_percentile'].iloc[i], xmin=0, xmax=jmax, color='black', linestyle=':', alpha=0.5, zorder=2, label=f"40th PERCENTILE")
 
-        ax.plot(dates, df['100hr_DFM_mean'], color='blue', alpha=1)
-        ax.plot(dates, df['100hr_DFM_max'], color='darkgreen', alpha=0.4)
-        ax.plot(dates, df['100hr_DFM_min'], color='darkorange', alpha=0.4)
-        ax.fill_between(dates, df['100hr_DFM_mean'], df['100hr_DFM_max'], color='green', alpha=0.25, where=(df['100hr_DFM_max'] > df['100hr_DFM_mean']))
-        ax.fill_between(dates, df['100hr_DFM_mean'], df['100hr_DFM_min'], color='orange', alpha=0.25, where=(df['100hr_DFM_min'] < df['100hr_DFM_mean']))
+        ax.axhspan(0, percentiles['100hr_DFM_3_percentile'].iloc[i], color='saddlebrown', alpha=0.2, label=f"0TH-3RD PERCENTILE")
+        ax.axhspan(percentiles['100hr_DFM_3_percentile'].iloc[i], percentiles['100hr_DFM_10_percentile'].iloc[i], color='peru', alpha=0.2, label=f"3RD-10TH PERCENTILE")
+        ax.axhspan(percentiles['100hr_DFM_10_percentile'].iloc[i], percentiles['100hr_DFM_20_percentile'].iloc[i], color='orange', alpha=0.2, label=f"10TH-20TH PERCENTILE")
+        ax.axhspan(percentiles['100hr_DFM_20_percentile'].iloc[i], percentiles['100hr_DFM_40_percentile'].iloc[i], color='gold', alpha=0.2, label=f"20TH-40TH PERCENTILE")
+        ax.axhspan(percentiles['100hr_DFM_40_percentile'].iloc[i], 30, color='lime', alpha=0.2, label=f"40TH-100TH PERCENTILE")
+
+        ax.plot(df_data['julian_date'], df_data['f100_mean'], color='blue', alpha=1, label=f"OBSERVED")
+        ax.plot(df_climo_avg['julian_date'], df_climo_avg['f100_avg'], color='gray', alpha=1, label=f"AVERAGE")
+        ax.plot(df_climo_min['julian_date'], df_climo_min['f100_min'], color='red', alpha=1, label=f"MAX")
+        
+        plt.legend(loc="upper left", fontsize="xx-small")
 
         fig.savefig(f"{path}/{fname}", bbox_inches='tight')
         psa = psa + 1
-        #except Exception as e:
-         #   pass
 
-    
     print(f"100-HR Fuels Charts Saved To {path_print}")
 
         
+def create_psa_1000hr_fuels_charts(gacc_region, number_of_years_for_averages=15, fuel_model='Y', start_date=None, data=False):
     
+    gacc_region = gacc_region.upper()
+
+    path, path_print = file_functions.get_fuels_charts_paths(gacc_region, '1000hr Dead Fuel')
+
+    if data == False:
+        FEMS.get_raws_sig_data(gacc_region, number_of_years_for_averages, fuel_model, start_date)
+        FEMS.get_nfdrs_forecast_data(gacc_region, fuel_model)
+    else:
+        pass
+        
+    get_psa_percentiles(gacc_region)
+    station_stats(gacc_region)
+    get_stats(gacc_region)
+    get_psa_climatology(gacc_region)
+    sort_data_by_psa(gacc_region)
+
+    data_dir = f"FEMS Data/{gacc_region}/PSA Data"
+    percentiles_dir = f"FEMS Data/{gacc_region}/PSA Percentiles"
+    climo_avg_dir = f"FEMS Data/{gacc_region}/PSA Climo/AVG"
+    climo_min_dir = f"FEMS Data/{gacc_region}/PSA Climo/MIN"
+
+    percentiles = pd.read_csv(f"FEMS Data/{gacc_region}/PSA Percentiles/PSA_Percentiles.csv")
+
+    leap = isleap(utc_time.year)
+    if start_date == None:
+        if leap == False:
+            days = number_of_years_for_averages * 365
+        else:
+            days = number_of_years_for_averages * 366
+        start_date = utc_time - timedelta(days=days)
+        start_year = start_date.year
+        xmin = start_date
+        xmax = utc_time
+
+    else:
+        start_date = start_date
+        start_year = f"{start_date[0]}{start_date[1]}{start_date[2]}{start_date[3]}"
+        start_month = f"{start_date[5]}{start_date[6]}"
+        start_day = f"{start_date[8]}{start_date[9]}"
+        xmin = datetime(int(start_year), int(start_month), int(start_day))
+        xmax = utc_time
+    
+    psa = 1
+
+    if os.path.exists(f"{data_dir}/.ipynb_checkpoints"):
+        shutil.rmtree(f"{data_dir}/.ipynb_checkpoints")
+    else:
+        pass
+
+    if os.path.exists(f"{data_dir}/.ipynb_checkpoints"):
+        shutil.rmtree(f"{data_dir}/.ipynb_checkpoints")
+    else:
+        pass
+
+    files = os.listdir(f"{data_dir}")
+    psa = 1
+    for i in range(0, len(files)):
+
+        fname = f"PSA {psa}.png"
+
+        df_data = pd.read_csv(f"{data_dir}/{files[i]}") 
+        df_climo_avg = pd.read_csv(f"{climo_avg_dir}/{files[i]}") 
+        df_climo_min = pd.read_csv(f"{climo_min_dir}/{files[i]}") 
+
+        dates = pd.to_datetime(df_data['dates'])
+
+        fig = plt.figure(figsize=(12,12))
+
+        ax = fig.add_subplot(1, 1, 1)
+
+        ax.xaxis.set_major_formatter(md.DateFormatter('%m-%d'))
+        plt.title(f"{gacc_region} 1000-HR Dead Fuel Moisture: PSA {psa}", fontsize=12, fontweight='bold', loc='left')
+        plt.title(f"Period Of Record: {start_year} - {utc_time.year}", fontsize=10, fontweight='bold', loc='right')
+        ax.text(0.01, -0.05, "Plot Created With FireWxPy (C) Eric J. Drewitz " +utc_time.strftime('%Y')+" | Data Source: USDA/FEMS", transform=ax.transAxes, fontsize=8, fontweight='bold', bbox=props)
+        ax.text(0.405, 0.98, f"Valid Date: {dates.iloc[-2].strftime("%m/%d/%Y")}", transform=ax.transAxes, fontsize=8, color='white', fontweight='bold', bbox=date_box)
+
+        if leap == True:
+            jmax = 366
+        else:
+            jmax = 365
+        
+        ax.set_xlim(0, jmax)
+        ax.set_ylim(0, 30)
+        
+        ax.hlines(percentiles['1000hr_DFM_3_percentile'].iloc[i], xmin=0, xmax=jmax, color='black', linestyle='-', alpha=0.5, zorder=2, label=f"3rd PERCENTILE")
+        ax.hlines(percentiles['1000hr_DFM_10_percentile'].iloc[i], xmin=0, xmax=jmax, color='black', linestyle='--', alpha=0.5, zorder=2, label=f"10th PERCENTILE")
+        ax.hlines(percentiles['1000hr_DFM_20_percentile'].iloc[i], xmin=0, xmax=jmax, color='black', linestyle='-.', alpha=0.5, zorder=2, label=f"20th PERCENTILE")
+        ax.hlines(percentiles['1000hr_DFM_40_percentile'].iloc[i], xmin=0, xmax=jmax, color='black', linestyle=':', alpha=0.5, zorder=2, label=f"40th PERCENTILE")
+
+        ax.axhspan(0, percentiles['1000hr_DFM_3_percentile'].iloc[i], color='saddlebrown', alpha=0.2, label=f"0TH-3RD PERCENTILE")
+        ax.axhspan(percentiles['1000hr_DFM_3_percentile'].iloc[i], percentiles['1000hr_DFM_10_percentile'].iloc[i], color='peru', alpha=0.2, label=f"3RD-10TH PERCENTILE")
+        ax.axhspan(percentiles['1000hr_DFM_10_percentile'].iloc[i], percentiles['1000hr_DFM_20_percentile'].iloc[i], color='orange', alpha=0.2, label=f"10TH-20TH PERCENTILE")
+        ax.axhspan(percentiles['1000hr_DFM_20_percentile'].iloc[i], percentiles['1000hr_DFM_40_percentile'].iloc[i], color='gold', alpha=0.2, label=f"20TH-40TH PERCENTILE")
+        ax.axhspan(percentiles['1000hr_DFM_40_percentile'].iloc[i], 30, color='lime', alpha=0.2, label=f"40TH-100TH PERCENTILE")
+
+        ax.plot(df_data['julian_date'], df_data['f1000_mean'], color='blue', alpha=1, label=f"OBSERVED")
+        ax.plot(df_climo_avg['julian_date'], df_climo_avg['f1000_avg'], color='gray', alpha=1, label=f"AVERAGE")
+        ax.plot(df_climo_min['julian_date'], df_climo_min['f1000_min'], color='red', alpha=1, label=f"MAX")
+        
+        plt.legend(loc="upper left", fontsize="xx-small")
+
+        fig.savefig(f"{path}/{fname}", bbox_inches='tight')
+        psa = psa + 1
+
+    print(f"1000-HR Fuels Charts Saved To {path_print}")
