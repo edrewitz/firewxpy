5acea20ab6568a99366cfc6ce839733d02b11a66
Eric Drewitz <101157849+edrewitz@users.noreply.github.com>
2025-05-17 13:38:25 -0700

    Update fuels_graphics.py

--- firewxpy/fuels_graphics.py
+++ firewxpy/fuels_graphics.py
@@ -279,6 +279,41 @@ def get_psa_ids(gacc_region):
     
 
 def create_psa_100hr_fuels_charts(gacc_region, number_of_years_for_averages=15, fuel_model='Y', start_date=None, data=False):
+
+    """
+    This function plots the 100hr dead fuel moisture for each Predictive Services Area for a specific Geographic Area Coordination Center. 
+
+    Required Arguments:
+
+    1) gacc_region (String) - The 4-letter GACC abbreviation
+
+    Optional Arguments: 
+
+    1) number_of_years_for_averages (Integer) - Default = 15. The number of years for the average values to be calculated on. 
+
+    3) fuel_model (String) - Default = 'Y'. The fuel model being used. 
+       Fuel Models List:
+
+       Y - Timber
+       X - Brush
+       W - Grass/Shrub
+       V - Grass
+       Z - Slash 
+
+    4) start_date (String) - Default = None. If the user wishes to use a selected start date as the starting point enter the start_date
+       as a string in the following format: YYYY-mm-dd
+
+    5) data (Boolean) - Default = False. When set to False, the function will download the new data. 
+    
+       When set to True the function will use the existing data and not download new data. 
+
+       When making multiple types of plots, I recommend setting every function to have data=True except for the first function called in the script. 
+
+    Returns
+    -------
+
+    A graphic showing the 100hr dead fuel moisture for each PSA for a specific GACC saved to: f:Fuels Maps/{gacc_region}/100hr Dead Fuel
+    """
     
     gacc_region = gacc_region.upper()
 
@@ -434,6 +469,41 @@ def create_psa_100hr_fuels_charts(gacc_region, number_of_years_for_averages=15, 
 
         
 def create_psa_1000hr_fuels_charts(gacc_region, number_of_years_for_averages=15, fuel_model='Y', start_date=None, data=False):
+
+    """
+    This function plots the 1000hr dead fuel moisture for each Predictive Services Area for a specific Geographic Area Coordination Center. 
+
+    Required Arguments:
+
+    1) gacc_region (String) - The 4-letter GACC abbreviation
+
+    Optional Arguments: 
+
+    1) number_of_years_for_averages (Integer) - Default = 15. The number of years for the average values to be calculated on. 
+
+    3) fuel_model (String) - Default = 'Y'. The fuel model being used. 
+       Fuel Models List:
+
+       Y - Timber
+       X - Brush
+       W - Grass/Shrub
+       V - Grass
+       Z - Slash 
+
+    4) start_date (String) - Default = None. If the user wishes to use a selected start date as the starting point enter the start_date
+       as a string in the following format: YYYY-mm-dd
+
+    5) data (Boolean) - Default = False. When set to False, the function will download the new data. 
+    
+       When set to True the function will use the existing data and not download new data. 
+
+       When making multiple types of plots, I recommend setting every function to have data=True except for the first function called in the script. 
+
+    Returns
+    -------
+
+    A graphic showing the 100hr dead fuel moisture for each PSA for a specific GACC saved to: f:Fuels Maps/{gacc_region}/1000hr Dead Fuel
+    """
     
     gacc_region = gacc_region.upper()
 
@@ -589,6 +659,41 @@ def create_psa_1000hr_fuels_charts(gacc_region, number_of_years_for_averages=15,
 
 
 def create_psa_erc_fuels_charts(gacc_region, number_of_years_for_averages=15, fuel_model='Y', start_date=None, data=False):
+
+    """
+    This function plots the Energy Release Component (ERC) Values for each Predictive Services Area for a specific Geographic Area Coordination Center. 
+
+    Required Arguments:
+
+    1) gacc_region (String) - The 4-letter GACC abbreviation
+
+    Optional Arguments: 
+
+    1) number_of_years_for_averages (Integer) - Default = 15. The number of years for the average values to be calculated on. 
+
+    3) fuel_model (String) - Default = 'Y'. The fuel model being used. 
+       Fuel Models List:
+
+       Y - Timber
+       X - Brush
+       W - Grass/Shrub
+       V - Grass
+       Z - Slash 
+
+    4) start_date (String) - Default = None. If the user wishes to use a selected start date as the starting point enter the start_date
+       as a string in the following format: YYYY-mm-dd
+
+    5) data (Boolean) - Default = False. When set to False, the function will download the new data. 
+    
+       When set to True the function will use the existing data and not download new data. 
+
+       When making multiple types of plots, I recommend setting every function to have data=True except for the first function called in the script. 
+
+    Returns
+    -------
+
+    A graphic showing the ERCs for each PSA for a specific GACC saved to: f:Fuels Maps/{gacc_region}/ERC
+    """
     
     gacc_region = gacc_region.upper()
 
@@ -724,6 +829,41 @@ def create_psa_erc_fuels_charts(gacc_region, number_of_years_for_averages=15, fu
     print(f"ERC Fuels Charts Saved To {path_print}")
 
 def create_psa_bi_fuels_charts(gacc_region, number_of_years_for_averages=15, fuel_model='Y', start_date=None, data=False):
+
+    """
+    This function plots the Burning Index (BI) for each Predictive Services Area for a specific Geographic Area Coordination Center. 
+
+    Required Arguments:
+
+    1) gacc_region (String) - The 4-letter GACC abbreviation
+
+    Optional Arguments: 
+
+    1) number_of_years_for_averages (Integer) - Default = 15. The number of years for the average values to be calculated on. 
+
+    3) fuel_model (String) - Default = 'Y'. The fuel model being used. 
+       Fuel Models List:
+
+       Y - Timber
+       X - Brush
+       W - Grass/Shrub
+       V - Grass
+       Z - Slash 
+
+    4) start_date (String) - Default = None. If the user wishes to use a selected start date as the starting point enter the start_date
+       as a string in the following format: YYYY-mm-dd
+
+    5) data (Boolean) - Default = False. When set to False, the function will download the new data. 
+    
+       When set to True the function will use the existing data and not download new data. 
+
+       When making multiple types of plots, I recommend setting every function to have data=True except for the first function called in the script. 
+
+    Returns
+    -------
+
+    A graphic showing the BIs for each PSA for a specific GACC saved to: f:Fuels Maps/{gacc_region}/BI
+    """
     
     gacc_region = gacc_region.upper()
 
@@ -859,6 +999,41 @@ def create_psa_bi_fuels_charts(gacc_region, number_of_years_for_averages=15, fue
     print(f"BI Fuels Charts Saved To {path_print}")
 
 def create_psa_sc_fuels_charts(gacc_region, number_of_years_for_averages=15, fuel_model='Y', start_date=None, data=False):
+
+    """
+    This function plots the Spread Component (SC) for each Predictive Services Area for a specific Geographic Area Coordination Center. 
+
+    Required Arguments:
+
+    1) gacc_region (String) - The 4-letter GACC abbreviation
+
+    Optional Arguments: 
+
+    1) number_of_years_for_averages (Integer) - Default = 15. The number of years for the average values to be calculated on. 
+
+    3) fuel_model (String) - Default = 'Y'. The fuel model being used. 
+       Fuel Models List:
+
+       Y - Timber
+       X - Brush
+       W - Grass/Shrub
+       V - Grass
+       Z - Slash 
+
+    4) start_date (String) - Default = None. If the user wishes to use a selected start date as the starting point enter the start_date
+       as a string in the following format: YYYY-mm-dd
+
+    5) data (Boolean) - Default = False. When set to False, the function will download the new data. 
+    
+       When set to True the function will use the existing data and not download new data. 
+
+       When making multiple types of plots, I recommend setting every function to have data=True except for the first function called in the script. 
+
+    Returns
+    -------
+
+    A graphic showing the SCs for each PSA for a specific GACC saved to: f:Fuels Maps/{gacc_region}/SC
+    """
     
     gacc_region = gacc_region.upper()
 
@@ -994,6 +1169,41 @@ def create_psa_sc_fuels_charts(gacc_region, number_of_years_for_averages=15, fue
     print(f"SC Fuels Charts Saved To {path_print}")
 
 def create_psa_ic_fuels_charts(gacc_region, number_of_years_for_averages=15, fuel_model='Y', start_date=None, data=False):
+
+    """
+    This function plots the Ignition Component (IC) for each Predictive Services Area for a specific Geographic Area Coordination Center. 
+
+    Required Arguments:
+
+    1) gacc_region (String) - The 4-letter GACC abbreviation
+
+    Optional Arguments: 
+
+    1) number_of_years_for_averages (Integer) - Default = 15. The number of years for the average values to be calculated on. 
+
+    3) fuel_model (String) - Default = 'Y'. The fuel model being used. 
+       Fuel Models List:
+
+       Y - Timber
+       X - Brush
+       W - Grass/Shrub
+       V - Grass
+       Z - Slash 
+
+    4) start_date (String) - Default = None. If the user wishes to use a selected start date as the starting point enter the start_date
+       as a string in the following format: YYYY-mm-dd
+
+    5) data (Boolean) - Default = False. When set to False, the function will download the new data. 
+    
+       When set to True the function will use the existing data and not download new data. 
+
+       When making multiple types of plots, I recommend setting every function to have data=True except for the first function called in the script. 
+
+    Returns
+    -------
+
+    A graphic showing the ICs for each PSA for a specific GACC saved to: f:Fuels Maps/{gacc_region}/IC
+    """
     
     gacc_region = gacc_region.upper()
 
