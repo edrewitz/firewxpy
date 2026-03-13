2f4818f04fe82011f4c8c8e235e4b1d5346cc7a5
edrewitz <drewitzwx@gmail.com>
2026-03-12 21:03:19 -0700

    checkpoint - geometry

---
+++ src/firewxpy/utils/geometry.py
@@ -0,0 +1,89 @@
+"""
+This file hosts the functions that import shapefiles/GEOJSON associated with the demarcation system (reference system) on the map.
+
+(C) Eric J. Drewitz 2024-2026
+"""
+
+
+from shapeography import(
+    client,
+    unzip,
+    geometry
+)
+
+
+def import_shapefile_from_web(url,
+                                path, 
+                                filename, 
+                                proxies, 
+                                chunk_size,
+                                notifications, 
+                                refresh,
+                                file_extension,
+                                file_path,
+                                edgecolor):
+    
+    """
+    This function downloads and imports the geometry from a shapefile hosted on the web.
+    
+    Required Arguments:
+    
+    1) url (String) - The download URL to the file. 
+    
+    2) path (String) - The directory where the file is saved to. 
+    
+    3) filename (String) - The name the user wishes to save the file as. 
+    
+    4) proxies (dict or None) - If the user is using proxy server(s), the user must change the following:
+
+       proxies=None ---> proxies={
+                               'http':'http://your-proxy-address:port',
+                               'https':'http://your-proxy-address:port'
+                               }
+                        
+        get_shapefiles(url, path, filename, proxies=proxies)
+                        
+    5) chunk_size (Integer) - The size of the chunks when writing the GRIB/NETCDF data to a file.
+    
+    6) notifications (String) - Notification when a file is downloaded and saved to {path}
+    
+    7) refresh (Boolean) - When set to True, the branch that hosts the shapefiles files is completely
+       cleaned out and a new set of shapefiles is downloaded with each run. This is recommended for those using 
+       shapeography in automated tasks to account for periodic shapefile updates on the servers that host the shapefiles.
+    
+    8) file_extension (String) - Default='.zip'. - The extension of the zip file. 
+    
+        Supported zip file extentions
+        -----------------------------
+            
+            1) .zip
+            2) .gz
+            3) .tar.gz
+            4) .tar
+            
+    9) file_path (String) - The full filepath to the shapefile. 
+    
+    10) edgecolor (String) - The color of the bordr demarcations on the map. 
+    
+    Returns
+    -------
+    
+    The geometry of a shapefile to plot with cartopy.    
+    """
+    
+    client.get_shapefiles(url,
+                     path,
+                     filename,
+                     proxies=proxies,
+                     chunk_size=chunk_size,
+                     notifications=notifications,
+                     refresh=refresh)
+    
+    unzip.extract_files(path,
+                        file_extension=file_extension)
+    
+    shapes = geometry.cartopy_shapefeature(file_path,
+                                           edgecolor=edgecolor)
+    
+    return shapes
+    
