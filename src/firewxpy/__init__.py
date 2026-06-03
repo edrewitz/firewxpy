f1eb059f7b1e845b10de0cdce2d766bdb12aa4d3
edrewitz <drewitzwx@gmail.com>
2026-06-02 18:15:15 -0700

    Updated __init__.py files

--- src/firewxpy/__init__.py
+++ src/firewxpy/__init__.py
@@ -1,2 +1,19 @@
+# (C) Eric J. Drewitz 2024 - 2026
+
+# - Upper Air
+# - Observed Soundings
+# - Observed Vertical Profiles
 import firewxpy.observations.upper_air.soundings as observed_soundings
 import firewxpy.observations.upper_air.profiles as observed_vertical_profiles
+
+# - Real Time Mesoscale Analysis (RTMA)
+# - RTMA CONUS
+# - RTMA Comparison CONUS
+# - RTMA Alaska
+# - RTMA Comparison Alaska
+# - RTMA Hawaii
+# - RTMA Comparison Hawaii
+import firewxpy.rtma.conus.rtma as rtma_conus
+import firewxpy.rtma.conus.rtma_comparison as rtma_comparison_conus
+import firewxpy.rtma.alaska.rtma as rtma_alaska
+import firewxpy.rtma.alaska.rtma_comparison as rtma_comparison_alaska
--- src/firewxpy/rtma/alaska/__init__.py
+++ src/firewxpy/rtma/alaska/__init__.py
@@ -1,1 +1,2 @@
 import firewxpy.rtma.alaska.rtma as rtma_alaska
+import firewxpy.rtma.alaska.rtma_comparison as rtma_comparison_alaska
