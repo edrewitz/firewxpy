8fdb0323357ebe63e509ea40909f1d446e396062
edrewitz <drewitzwx@gmail.com>
2026-06-14 10:39:29 -0700

    update docstrings

--- src/firewxpy/rtma/alaska/rtma_comparison.py
+++ src/firewxpy/rtma/alaska/rtma_comparison.py
@@ -8,7 +8,7 @@ This file hosts the functions for the Alaska Real Time Mesoscale Analysis (RTMA)
 
 ######################################### Documentation ###############################################
 
-These functions plot the Real Time Mesoscale Analysis for Alaska.
+These functions plot the Real Time Mesoscale Analysis Comparison for Alaska.
 
 - FireWxPy >= 2.0 Brings much new customization to each set of graphics where everything is customizable.
 - FireWxPy >= 2.0 Uses the WxData and Shapeography packages as a back-end for automating these graphics.
--- src/firewxpy/rtma/hawaii/rtma_comparison.py
+++ src/firewxpy/rtma/hawaii/rtma_comparison.py
@@ -8,7 +8,7 @@ This file hosts the functions for the Alaska Real Time Mesoscale Analysis (RTMA)
 
 ######################################### Documentation ###############################################
 
-These functions plot the Real Time Mesoscale Analysis for Hawaii.
+These functions plot the Real Time Mesoscale Analysis Comparison for Hawaii.
 
 - FireWxPy >= 2.0 Brings much new customization to each set of graphics where everything is customizable.
 - FireWxPy >= 2.0 Uses the WxData and Shapeography packages as a back-end for automating these graphics.
